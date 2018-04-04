d7334687e713e62572d08a4caa99d97068d3fe3f
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-03 20:15:17 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -38,24 +38,7 @@ class fun:
         await msg.edit(content="IM A MEME BOYZZZZ LMAOOOO XDDD")
         await asyncio.sleep(4)
         await msg.edit(content=f"lmao xd")   
-         
-       
-@bot.command()
-async def say(ctx, *, message:str):
-    """Speak as me!"""
-    await ctx.message.delete()
-    await ctx.send(message)        
-        
-       
-@bot.command()
-async def github(ctx):
-    """Get my github link"""
-    await ctx.send("Here is my github: http://bit.ly/2ogUv2T")
-
-@bot.command()
-async def lit(ctx):
-    """You lit? Use this command!"""
-    await ctx.send("**Only the lit people can listen to this! http://bit.ly/2r3aFyX**")	
+            
     
     
 def setup(bot):
