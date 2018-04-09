27f6c3364bc4e734562524bde9889141d3501cfe
IceeMC <31800435+IceeMC@users.noreply.github.com>
2018-04-08 19:06:43 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -75,8 +75,8 @@ class fun:
         '''Expose someone!'''
         if user is None:
             return await ctx.send(":no_entry_sign: **You need to mention a user.**")
-        roasts = ["likes https://pornhub.com/ I found it in his history", "is super gay.", "likes your mom.", "copies code from my creator.", "is Adolf Hitler's son.", "is 97% gay :gay_pride_flag:", "likes GayGrok Better then CodeGrok. https://discord.gg/3QAxxfM "]
-        await ctx.send(f"{lol} {random.choice(exposes)}")
+        roasts = ["likes https://pornhub.com/, I found it in his history", "is super gay.", "likes your mom.", "copies code from my creator.", "is Adolf Hitler's son.", "is 97% gay :gay_pride_flag:", "likes GayGrok Better then CodeGrok. https://discord.gg/3QAxxfM "]
+        await ctx.send(f"{user.mention} {random.choice(exposes)}")
     
     
 def setup(bot):
