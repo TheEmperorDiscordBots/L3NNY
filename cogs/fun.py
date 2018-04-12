57ec55fda7ef9d2cd29aa2118fdc0d69361881e5
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-11 22:01:16 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -61,5 +61,27 @@ class fun:
             return await ctx.send(f":no_entry_sign: **{user}**, is not a valid username or mention")
     
     
+    @commands.command()
+    async def serverstats(self, ctx):
+        """Just server stats"""
+        guild = ctx.guild
+        roles = [x.name for x in guild.roles]
+        role_length = len(roles)
+        roles = ', '.join(roles)
+        channels = len(guild.channels)
+        time = str(guild.created_at.strftime("%b %m, %Y, %A, %I:%M %p"))         
+        em = discord.Embed(description= "The Statistics for this server", title='Server Stats', colour=0xffffff)
+        em.set_thumbnail(url=guild.icon_url)
+        em.add_field(name='__Server __', value=str(guild.name))
+        em.add_field(name='__Owner__', value=str(guild.owner))
+        em.add_field(name='__Owner ID__', value=guild.owner_id) 
+        em.add_field(name='__Members__', value=str(guild.member_count))
+        em.add_field(name='__Total Channels__', value=str(channels))
+        em.add_field(name='__Region__', value='%s' % str(guild.region))
+        em.add_field(name='__ Total Roles__', value='%s' % str(role_length))
+        em.set_footer(text='Created - %s' % time)        
+        await ctx.send(embed=em)
+
+    
 def setup(bot):
     bot.add_cog(fun(bot))
