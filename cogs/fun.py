03c64f7f53a03f45714a60352e73bfc1ce2e6f28
TheEmperorThatCodes?? <35822194+TheEmporor@users.noreply.github.com>
2018-04-14 19:29:52 -0500

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -35,37 +35,7 @@ class fun:
         await msg.edit(content=f"lmao xd")   
             
     
-    @commands.command(aliases=['g'])
-    async def google(self, ctx, *, query):
-        """Searches google and gives you top result."""
-        await ctx.trigger_typing()
-        try:
-            card, entries = await self.get_google_entries(query)
-        except RuntimeError as e:
-            await ctx.send(str(e))
-        else:
-            if card:
-                value = '\n'.join(f'[{title}]({url.replace(")", "%29")})' for url,
-                                  title in entries[:3])
-                if value:
-                    card.add_field(name='Search Results', value=value, inline=False)
-                return await ctx.send(embed=card)
-
-            if len(entries) == 0:
-                return await ctx.send('No results found... sorry.')
-
-            next_two = [x[0] for x in entries[1:3]]
-            first_entry = entries[0][0]
-            if first_entry[-1] == ')':
-                first_entry = first_entry[:-1] + '%29'
-
-            if next_two:
-                formatted = '\n'.join(f'<{x}>' for x in next_two)
-                msg = f'{first_entry}\n\n**See also:**\n{formatted}'
-            else:
-                msg = first_entry
-
-            await ctx.send(msg)
+   
     
     
     @commands.command()
@@ -115,20 +85,6 @@ class fun:
         em.set_footer(text='Created - %s' % time)        
         await ctx.send(embed=em)
 
-        
-    @commands.command(aliases=['yt', 'vid', 'video'])
-    async def yt(self, ctx, *, search):
-        """Search for videos on YouTube"""
-        search = search.replace(' ', '+').lower()
-        response = requests.get(f"https://www.youtube.com/results?search_query={search}").text
-        result = BeautifulSoup(response, "lxml")
-        dir_address = f"{result.find_all(attrs={'class': 'yt-uix-tile-link'})[0].get('href')}"
-        output=f"**Top Result:**\nhttps://www.youtube.com{dir_address}"
-        try:
-            await ctx.send(output)
-            await ctx.message.delete()
-        except discord.Forbidden:
-            pass
         
            
 def setup(bot):
